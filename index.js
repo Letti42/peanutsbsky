11e6dce1d5f80acc02720cf9e1260ab7c7439373
Letti <78980794+Letti42@users.noreply.github.com>
2024-11-16 10:56:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { getComic } from "./comic.js";
 import { login, uploadImage, createPost } from "./bsky.js";
 import fs from 'fs';
 
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 const agent = await login();
 let data = await getComic();
 let embed = await uploadImage(data.bytes);
@@ -15,16 +17,14 @@ if(post)
 
 
 
-
-
-
 function constructPost(data, embed){
     if(!embed)throw new Error("embed undefined @ constructPost");
 
     let date = reformatFullDate(data.date);
-    let text = getRandomTextTemplate();
+    //let text = getRandomTextTemplate();
+    let text = `${months[Number(date.split("/")[0])-1]} ${Number(date.split("/")[1])}, ${data.yearReleased}\n${data.currentYear-data.yearReleased} years ago `;
 
-    text = text.replace("{date}", date).replace("{years}", data.currentYear - data.yearReleased);
+    //text = text.replace("{date}", date).replace("{years}", data.currentYear - data.yearReleased);
     text += getRandomEmojis();
 
     let post = {
@@ -36,7 +36,7 @@ function constructPost(data, embed){
             "$type": "app.bsky.embed.images",
             "images":[
                 {
-                    "alt":"Peanuts comic from "+data.date,
+                    "alt":"Peanuts comic from "+date,
                     "image":embed.blob
                 }
             ]
@@ -49,14 +49,20 @@ function constructPost(data, embed){
 
 
 function getRandomTextTemplate(){
-    var texts = fs.readFileSync("text_templates", "utf-8").split("\r").join("").split('\n'); //fck yall idk regex!!
-    let randText = texts[Math.floor(Math.random() * texts.length)];
+    // var texts = fs.readFileSync("text_templates", "utf-8").split("\r").join("").split('\n'); //fck yall idk regex!!
+    // let randText = texts[Math.floor(Math.random() * texts.length)];
+    
+    //back to boring month / date / year 😔
+
 
     return randText;
 }
 
 function getRandomEmojis(){
     var emojis = fs.readFileSync("emojis", "utf-8").split("\r").join("").split('\n'); //no regex merchant
+    return emojis[Math.floor(Math.random() * emojis.length)];
+    
+    
     var emojiText = String();
 
     for(let i = 0; i < 2; i++){
